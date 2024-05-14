da5a1b1b2e17001cd4d0be907e4a9e95e26bc349
emanoelle9 <emanoelleferreiramateus@gmail.com>
2024-05-14 10:56:23 -0300

    feat: Estrutura inicialde projeto e janelas

---
+++ janelas/janela_cadastro.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+
+class CadastroVeiculo(ctk.CTkToplevel):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.title("Cadastro de Veiculos")
+        self.geometry("800x600")
+        self.resizable(False, False)
+
+        self.label_exemplo = ctk.CTkLabel(self, text='Exemplo')
+        self.label_exemplo.pack()
+
+
+    
+    
